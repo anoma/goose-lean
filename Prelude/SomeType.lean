c7406774e1175e3983c413e53db0fc512b6d1ddd
Lukasz Czajka <lukasz@heliax.dev>
2025-07-07 11:43:18 +0200

    SomeType field naming changes

--- Prelude/SomeType.lean
+++ Prelude/SomeType.lean
@@ -3,20 +3,20 @@ import Prelude.TypeRep
 
 structure SomeType where
   type : Type u
-  [rep : TypeRep type]
-  [beq : BEq type]
+  [instTypeRep : TypeRep type]
+  [instBEq : BEq type]
 
 instance SomeType.hasTypeRep : TypeRep SomeType where
   rep := Rep.atomic "SomeType"
 
 instance SomeType.hasBEq : BEq SomeType where
-  beq a b := a.rep.rep == b.rep.rep
+  beq a b := a.instTypeRep.rep == b.instTypeRep.rep
 
 instance {A : SomeType} : TypeRep A.type where
-  rep := A.rep.rep
+  rep := A.instTypeRep.rep
 
 instance {A : SomeType} : BEq A.type where
-  beq := A.beq.beq
+  beq := A.instBEq.beq
 
 def SomeType.cast {A B : SomeType} (a : A.type) : Option B.type :=
-  tryCast (repA := A.rep) (repB := B.rep) a
+  tryCast (repA := A.instTypeRep) (repB := B.instTypeRep) a
