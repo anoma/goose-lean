f3f5b0ad2b00ce25d2392938a6589a791ee57b75
Jan Mas Rovira <janmasrovira@gmail.com>
2025-07-11 16:10:31 +0200

    kudos app

---
+++ Apps/Kudos.lean
@@ -0,0 +1,110 @@
+import AVM
+import Applib
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Tactic.DeriveFintype
+
+open Applib
+
+/-- Public name of someone -/
+abbrev PublicIden := Anoma.NullifierKeyCommitment
+
+structure Kudos where
+  quantity : Nat
+  originator : PublicIden
+  owner : PublicIden
+  deriving DecidableEq
+
+namespace Kudos
+
+inductive Methods where
+  | Split : Methods
+  | Transfer : Methods
+  deriving DecidableEq, Fintype, Repr
+
+inductive Constructors where
+  | Mint : Constructors
+  deriving DecidableEq, Fintype, Repr
+
+deriving instance Inhabited for Kudos
+
+open AVM
+
+instance hasTypeRep : TypeRep Kudos where
+  rep := Rep.atomic "Kudos"
+
+structure MintArgs where
+  key : Anoma.NullifierKey
+  quantity : Nat
+  deriving DecidableEq
+
+instance MintArgs.hasTypeRep : TypeRep MintArgs where
+  rep := Rep.atomic "MintArgs"
+
+structure SplitArgs where
+  quantities : List Nat
+  deriving DecidableEq
+
+instance SplitArgs.hasTypeRep : TypeRep SplitArgs where
+  rep := Rep.atomic "SplitArgs"
+
+structure TransferArgs where
+  newOwner : PublicIden
+  deriving DecidableEq
+
+instance TransferArgs.hasTypeRep : TypeRep TransferArgs where
+  rep := Rep.atomic "TransferArgs"
+
+def lab : Class.Label where
+  PrivateFields := ⟨PublicIden⟩
+  PublicFields := ⟨Unit⟩
+  MethodId := Methods
+  MethodArgs := fun
+    | Methods.Split => ⟨SplitArgs⟩
+    | Methods.Transfer => ⟨TransferArgs⟩
+  ConstructorId := Constructors
+  ConstructorArgs := fun
+    | Constructors.Mint => ⟨MintArgs⟩
+  name := "Kudos"
+
+def toObject (c : Kudos) : Object lab where
+  publicFields := Unit.unit
+  quantity := c.quantity
+  privateFields := c.originator
+  nullifierKeyCommitment := c.owner
+
+def fromObject (o : Object lab) : Option Kudos := do
+  let key <- o.nullifierKeyCommitment
+  some { owner := key
+         quantity := o.quantity
+         originator := o.privateFields }
+
+instance hasIsObject : IsObject Kudos where
+  lab := lab
+  toObject := Kudos.toObject
+  fromObject := Kudos.fromObject
+  roundTrip : Kudos.fromObject ∘ Kudos.toObject = some := by rfl
+
+def kudosMint : @Class.Constructor lab Constructors.Mint := defConstructor Kudos
+  (body := fun (args : MintArgs) => { quantity := args.quantity
+                                      owner := args.key.commitment
+                                      originator := args.key.commitment })
+  (invariant := fun (_args : MintArgs) => true)
+
+def kudosSplit : @Class.Method lab Methods.Split := defMethod Kudos
+  (body := fun (self : Kudos) (args : SplitArgs) =>
+    let mk (q : Nat) : Kudos := {self with quantity := q}
+    List.map (IsObject.toAnObject ∘ mk) args.quantities)
+  (invariant := fun (_self : Kudos) (_args : SplitArgs) => true)
+
+def kudosTransfer : @Class.Method lab Methods.Transfer := defMethod Kudos
+  (body := fun (self : Kudos) (args : TransferArgs) =>
+    [{self with owner := args.newOwner : Kudos}])
+  (invariant := fun (_self : Kudos) (_args : TransferArgs) => true)
+
+def kudosClass : Class lab where
+  constructors := fun
+    | Constructors.Mint => kudosMint
+  methods := fun
+    | Methods.Split => kudosSplit
+    | Methods.Transfer => kudosTransfer
+  invariant _ _ := True
