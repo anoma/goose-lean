246493aeaedd4519a106f90e7cab8c27c45b839b
Lukasz Czajka <lukasz@heliax.dev>
2025-07-18 11:30:11 +0200

    comments

--- AVM/Intent.lean
+++ AVM/Intent.lean
@@ -9,17 +9,18 @@ namespace AVM
     are compiled to resources with the conditions compiled to resource logics.
     Intent creation is compiled to transaction submission. -/
 structure Intent where
-  /-- The type of intent arguments. The arguments are stored in the `value`
-    field of the intent resource. They are not provided in the app data for any
-    resource logics. -/
+  /-- The type of intent arguments. The arguments are stored in the `label`
+    field of the intent resource (see `Class.DynamicLabel` in
+    `AVM/Class/Label.lean`). The intent arguments are not provided in the
+    app data for any resource logics. -/
   Args : SomeType
   /-- The unique label of the intent. The textual representation (via Repr) of
     the IntentId constructor corresponding to an intent must be equal to the
     intent's label. -/
   label : String
   /-- The intent condition checks if the desired objects were received. Given
-      intent arguments and provided objects, the intent condition is compiled to
-      the resource logic of the resource intent. -/
+    intent arguments and provided objects, the intent condition is compiled to
+    the resource logic of the resource intent. -/
   condition : Args.type → (provided : List SomeObject) → (received : List SomeObject) → Bool
 
 /-- Intent.ResourceData is stored in the `label` field of the intent resource. -/
