cf17aff28129919684239e3df7bf7e0c71ec8e1b
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2025-07-21 12:44:38 +0200

    Update AVM/Ecosystem/Member.lean

--- AVM/Ecosystem/Member.lean
+++ AVM/Ecosystem/Member.lean
@@ -4,6 +4,6 @@ namespace AVM
 
 structure Function {lab : Ecosystem.Label} (functionId : lab.FunctionId) where
   /-- Objects created in the function call. -/
-  created (selfs : functionId.Selves) (args : functionId.Args.type) : List SomeObject
+  created (selves : functionId.Selves) (args : functionId.Args.type) : List SomeObject
   /-- Extra function logic. -/
-  invariant (selfs : functionId.Selves) (args : functionId.Args.type) : Bool
+  invariant (selves : functionId.Selves) (args : functionId.Args.type) : Bool
