88b80a7927fb10310295d35b2c8383ee5185bfee
Jan Mas Rovira <janmasrovira@gmail.com>
2025-07-21 15:03:26 +0200

    remove end

--- Anoma/Transaction.lean
+++ Anoma/Transaction.lean
@@ -11,4 +11,3 @@ structure Transaction where
 def Transaction.generateDeltaProof (witness : DeltaWitness) (actions : List Action) : DeltaProof :=
   -- Placeholder implementation for generating a delta proof
   "DeltaProof for a transaction with " ++ toString (List.length actions) ++ " actions and signing key " ++ witness.signingKey
-end Anoma
