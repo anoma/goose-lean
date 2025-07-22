c6fe30e50bb89fdc537e69719d1f686594ffff4c
Lukasz Czajka <lukasz@heliax.dev>
2025-07-22 16:36:20 +0200

    improve do-notation handling

--- Prelude/LetTry.lean
+++ Prelude/LetTry.lean
@@ -3,40 +3,40 @@ syntax withPosition("let" "try" term (":" term)? ":=" term) optSemicolon(term) :
 syntax withPosition("let" "try" term (":" term)? ":=" term) optSemicolon(doSeq) : doElem
 syntax withPosition("let" "try" term (":" term)? "←" term)  optSemicolon(doSeq) : doElem
 syntax withPosition("let" "try" term (":" term)? ":=" term) "failwith" term optSemicolon(term) : term
-syntax withPosition("let" "try" term (":" term)? ":=" term) "failwith" term optSemicolon(doSeq) : doElem
-syntax withPosition("let" "try" term (":" term)? "←" term) "failwith" term optSemicolon(doSeq) : doElem
+syntax withPosition("let" "try" term (":" term)? ":=" term) "failwith" doSeq optSemicolon(doSeq) : doElem
+syntax withPosition("let" "try" term (":" term)? "←" term) "failwith" doSeq optSemicolon(doSeq) : doElem
 
 /-- The `let try x := ov; b` macro unwraps an `Option`, for `ov = some v` binds
   the value `v` to `x` in `b`, for `ov = none` returns `default`. The value
   returned on failure (when `ov = none`) can be specified with the `failwith`
   clause: `let try x := ov failwith failure; b`. The monadic version
-  `let try ← mov; b` can be used when `mov : m (Option V)` for some monad `m`.
-  The binder `x` can be given a type and it can be an arbitrary match pattern. -/
+  `let try x ← mov; b` can be used when `mov : m (Option V)` for some monad `m`.
+  The type of `x` can be specified and `x` can be an arbitrary match pattern. -/
 macro_rules
 | `(let try $x:term := $e:term ; $body) =>
   `(match ($e) with | none => default | some $x => $body)
 | `(let try $x:term : $t:term := $e:term ; $body) =>
   `(match ($e) with | none => default | some ($x : $t) => $body)
-| `(let try $x:term := $e:term failwith $r ; $body) =>
+| `(let try $x:term := $e:term failwith $r:term ; $body) =>
   `(match ($e) with | none => $r | some $x => $body)
-| `(let try $x:term : $t:term := $e:term failwith $r ; $body) =>
+| `(let try $x:term : $t:term := $e:term failwith $r:term ; $body) =>
   `(match ($e) with | none => $r | some ($x : $t) => $body)
 | `(doElem| let try $x:term := $e:term ; $body) =>
-  `(doElem| match ($e) with | none => pure default | some $x => $body)
+  `(doElem| match ($e) with | none => default | some $x => $body)
 | `(doElem| let try $x:term : $t:term := $e:term ; $body) =>
-  `(doElem| match ($e) with | none => pure default | some ($x : $t) => $body)
-| `(doElem| let try $x:term := $e:term failwith $r ; $body) =>
-  `(doElem| match ($e) with | none => pure $r | some $x => $body)
-| `(doElem| let try $x:term : $t:term := $e:term failwith $r ; $body) =>
-  `(doElem| match ($e) with | none => pure $r | some ($x : $t) => $body)
+  `(doElem| match ($e) with | none => default | some ($x : $t) => $body)
+| `(doElem| let try $x:term := $e:term failwith $r:doSeq ; $body) =>
+  `(doElem| match ($e) with | none => $r | some $x => $body)
+| `(doElem| let try $x:term : $t:term := $e:term failwith $r:doSeq ; $body) =>
+  `(doElem| match ($e) with | none => $r | some ($x : $t) => $body)
 | `(doElem| let try $x:term ← $e:term ; $body) =>
-  `(doElem| match ← $e with | none => pure default | some $x => $body)
+  `(doElem| match ← $e with | none => default | some $x => $body)
 | `(doElem| let try $x:term : $t:term ← $e:term ; $body) =>
-  `(doElem| match ← $e with | none => pure default | some ($x : $t) => $body)
-| `(doElem| let try $x:term ← $e:term failwith $r ; $body) =>
-  `(doElem| match ← $e with | none => pure $r | some $x => $body)
-| `(doElem| let try $x:term : $t:term ← $e:term failwith $r ; $body) =>
-  `(doElem| match ← $e with | none => pure $r | some ($x : $t) => $body)
+  `(doElem| match ← $e with | none => default | some ($x : $t) => $body)
+| `(doElem| let try $x:term ← $e:term failwith $r:doSeq ; $body) =>
+  `(doElem| match ← $e with | none => $r | some $x => $body)
+| `(doElem| let try $x:term : $t:term ← $e:term failwith $r:doSeq ; $body) =>
+  `(doElem| match ← $e with | none => $r | some ($x : $t) => $body)
 
 /-
 #eval let try x := some 42; some (x + 1)
