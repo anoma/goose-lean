02245ea51695fa6bfb4bdeb3248e83d907afbb87
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2025-07-23 18:32:58 +0200

    Dummy Resource refactor (#61)
    - Moves `dummyResource` and related functions to
    `Action/DummyResource.lean`
    - Fixes universe level annotations in `Anoma.Resource`

---
+++ AVM/Action/DummyResource.lean
@@ -0,0 +1,28 @@
+
+import Prelude
+import Anoma
+
+namespace AVM.Action
+
+private def dummyResourceLabel.{u} : ULift.{u} String :=
+  ULift.up.{u} "dummy-resource"
+
+/-- A dummy resource used in generated actions. -/
+def dummyResource (nonce : Anoma.Nonce) : Anoma.Resource.{u, v} :=
+  { Val := ⟨UUnit.{u}⟩,
+    Label := ⟨ULift.{v} String⟩,
+    label := dummyResourceLabel.{v},
+    quantity := 0,
+    value := UUnit.unit,
+    ephemeral := true,
+    nonce,
+    nullifierKeyCommitment := Anoma.NullifierKeyCommitment.universal }
+
+/-- Checks if a resource is a dummy resource. -/
+def isDummyResource (res : Anoma.Resource.{u, v}) : Bool :=
+  res.label === dummyResourceLabel.{v} && res.ephemeral && res.quantity == 0
+
+/-- The resource logic of any dummy resource. -/
+def dummyResourceLogic (args : Anoma.Logic.Args.{u, v} Unit) : Bool :=
+  let res : Anoma.Resource.{u, v} := args.self
+  isDummyResource res
