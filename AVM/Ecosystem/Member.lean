1cdf42c82be8a2e856d87f5df28c088f79a919df
Jan Mas Rovira <janmasrovira@gmail.com>
2025-07-24 11:01:56 +0200

    comments

--- AVM/Ecosystem/Member.lean
+++ AVM/Ecosystem/Member.lean
@@ -4,11 +4,13 @@ import AVM.Object.Consumable
 namespace AVM
 
 structure FunctionResult : Type (u + 1) where
+  /-- List of created objects -/
   created : List SomeObject
+  /-- List of objects destroyed. Destroyed elements will be balanced with automatically generated ephemeral resources -/
   destroyed : List SomeConsumableObject
 
 structure Function {lab : Ecosystem.Label} (functionId : lab.FunctionId) where
-  /-- Objects created in the function call. -/
+  /-- Computes the result of a function call. See `FunctionResult`. -/
   body (selves : functionId.Selves) (args : functionId.Args.type) : FunctionResult
   /-- Extra function logic. -/
   invariant (selves : functionId.Selves) (args : functionId.Args.type) : Bool
