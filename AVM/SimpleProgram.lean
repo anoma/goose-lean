607a8576a4d9002603582e4cb5e511e4d215312d
Lukasz Czajka <lukasz@heliax.dev>
2025-08-08 17:51:01 +0200

    Message

--- AVM/SimpleProgram.lean
+++ AVM/SimpleProgram.lean
@@ -1,2 +1,12 @@
 import Prelude
+import Anoma
 import AVM.Object
+import AVM.Message
+
+namespace AVM
+
+structure SimpleProgram (lab : Class.Label) where
+  /-- List of objects to fetch by object uid. -/
+  fetch : List Anoma.ObjectId
+  message : Message lab
+  actions : List Anoma.Action
