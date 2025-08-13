ac2cb5d06a79caecc33b90a7a43e703286f8e2dc
Lukasz Czajka <lukasz@heliax.dev>
2025-08-13 11:29:42 +0200

    update check

--- Prelude/Check.lean
+++ Prelude/Check.lean
@@ -1,12 +1,10 @@
 syntax withPosition("check" term) optSemicolon(term) : term
-syntax withPosition("checkM" term) optSemicolon(doSeq) : doElem
+syntax withPosition("check" term) optSemicolon(doSeq) : doElem
 
 /-- The `check a; b` macro returns `default` if `a` evaluates to `false`, or `b` if
   `a` evaluates to `true`. -/
 macro_rules
-| `(check $cond:term ; $body) =>
+| `(check $cond:term ; $body:term) =>
   `(if $cond then $body else default)
-
-macro_rules
-| `(doElem| checkM $cond:term ; $body) =>
-  `(doElem| if $cond then $body else default)
+| `(doElem| check $cond:term ; $body:doSeq) =>
+  `(doElem| if $cond then $body else pure default)
