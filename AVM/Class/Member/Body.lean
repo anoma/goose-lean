7d159b00bf08890ba435e81cdc7807127400210d
Łukasz Czajka <62751+lukaszcz@users.noreply.github.com>
2025-08-26 17:28:35 +0200

    Update AVM/Class/Member/Body.lean
    Co-authored-by: Jan Mas Rovira <janmasrovira@gmail.com>

--- AVM/Class/Member/Body.lean
+++ AVM/Class/Member/Body.lean
@@ -8,11 +8,38 @@ namespace AVM.Class
 /-- The parameters `params` represent objects fetched and new object ids
   generated in the body before the current statement. -/
 inductive Member.Body.{u} (lab : Ecosystem.Label) (ReturnType : Type u) : Task.Parameters.{u} → Type (u + 1) where
-  | constructor {params : Task.Parameters} (cid : lab.ClassId) (constrId : cid.label.ConstructorId) (args : params.Product → constrId.Args.type) (next : Member.Body lab ReturnType params.snocGenId) : Member.Body lab ReturnType params
-  | destructor {params : Task.Parameters} (cid : lab.ClassId) (destrId : cid.label.DestructorId) (selfId : params.Product → ObjectId) (args : params.Product → destrId.Args.type) (next : Member.Body lab ReturnType params) : Member.Body lab ReturnType params
-  | method {params : Task.Parameters} (cid : lab.ClassId) (methodId : cid.label.MethodId) (selfId : params.Product → ObjectId) (args : params.Product → methodId.Args.type) (next : Member.Body lab ReturnType params) : Member.Body lab ReturnType params
-  | fetch {params : Task.Parameters} (objId : params.Product → TypedObjectId) (next : Member.Body lab ReturnType (params.snocFetch objId)) : Member.Body lab ReturnType params
-  | return {params : Task.Parameters} (val : params.Product → ReturnType) : Member.Body lab ReturnType params
+  | constructor
+      {params : Task.Parameters}
+      (cid : lab.ClassId)
+      (constrId : cid.label.ConstructorId)
+      (args : params.Product → constrId.Args.type)
+      (next : Member.Body lab ReturnType params.snocGenId)
+      : Member.Body lab ReturnType params
+  | destructor
+      {params : Task.Parameters}
+      (cid : lab.ClassId)
+      (destrId : cid.label.DestructorId)
+      (selfId : params.Product → ObjectId)
+      (args : params.Product → destrId.Args.type)
+      (next : Member.Body lab ReturnType params)
+      : Member.Body lab ReturnType params
+  | method
+      {params : Task.Parameters}
+      (cid : lab.ClassId)
+      (methodId : cid.label.MethodId)
+      (selfId : params.Product → ObjectId)
+      (args : params.Product → methodId.Args.type)
+      (next : Member.Body lab ReturnType params)
+      : Member.Body lab ReturnType params
+  | fetch
+      {params : Task.Parameters}
+      (objId : params.Product → TypedObjectId)
+      (next : Member.Body lab ReturnType (params.snocFetch objId))
+      : Member.Body lab ReturnType params
+  | return
+      {params : Task.Parameters}
+      (val : params.Product → ReturnType)
+      : Member.Body lab ReturnType params
 
 /-- All body parameters - the parameters at the point of the return statement. -/
 def Member.Body.params {lab ReturnType params} (body : Member.Body lab ReturnType params) : Task.Parameters :=
