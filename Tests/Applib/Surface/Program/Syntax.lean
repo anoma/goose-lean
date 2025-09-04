92cec9b2a1f147af2f6bb7c2cfd61156d0c9915c
Lukasz Czajka <lukasz@heliax.dev>
2025-09-04 18:46:39 +0200

    syntax tests

--- Tests/Applib/Surface/Program/Syntax.lean
+++ Tests/Applib/Surface/Program/Syntax.lean
@@ -14,12 +14,18 @@ example (rx ry : Reference Counter) : Program Eco.lab Counter := ⟪
   return {x with count := x.count + y.count}
 ⟫
 
-example (rx ry : Reference Counter) : Program Eco.lab Unit := ⟪
+def mutualIncrement (rx ry : Reference Counter) (n : Nat) : Program Eco.lab Unit := ⟪
   x := fetch rx
   y := fetch ry
-  call Counter.Methods.Incr rx (x.count * 2 + y.count)
-  call Counter.Methods.Incr ry (y.count * 2 + x.count)
+  call Counter.Methods.Incr rx (x.count * n + y.count)
+  call Counter.Methods.Incr ry (y.count * n + x.count)
   return ()
+⟫
+
+example (self : TwoCounter) (n : Nat) : Program Eco.lab TwoCounter := ⟪
+  invoke mutualIncrement self.c1 self.c2 n
+  invoke mutualIncrement self.c2 self.c1 n
+  return self
 ⟫
 
 example : Program Eco.lab (Reference TwoCounter) := ⟪
