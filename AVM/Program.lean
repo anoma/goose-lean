145638400e6b81f3892f65e3e124cca771869bad
Jan Mas Rovira <janmasrovira@gmail.com>
2025-09-04 16:22:18 +0200

    fix termination

--- AVM/Program.lean
+++ AVM/Program.lean
@@ -61,25 +61,36 @@ def Program lab ReturnType := Σ n, Program' lab ReturnType n
 
 def Program'.result {lab : Ecosystem.Label} {ReturnType} {n : Nat} (prog : Program' lab ReturnType n)
   : Σ (params : Program.Parameters), params.Product → ReturnType :=
-  match n, prog with
-  | Nat.succ z, .constructor _ _ _ next =>
-    ⟨.genId (fun objId => result (n := z) (next objId) |>.1),
-      fun ⟨objId, vals⟩ => result (n := z) (next objId) |>.2 vals⟩
-  | Nat.succ m, .destructor _ _ _ _ next => next.result (n := m)
-  | Nat.succ m, .method _ _ _ _ next => next.result (n := m)
-  | Nat.succ m, .fetch objId next =>
-    ⟨.fetch objId (fun obj => result (n := m) (next obj) |>.1),
-      fun ⟨obj, vals⟩ => result (n := m) (next obj) |>.2 vals⟩
-  | Nat.succ m, .invoke _ p next =>
-    let ⟨pParams, pReturn⟩ := p.result (n := m)
+  match prog with
+  | .constructor (n := a) _ _ _ next =>
+    let tmp (prog : Program' lab ReturnType a)
+            : Σ (params : Program.Parameters.{0}), params.Product → ReturnType := Program'.result prog
+    ⟨.genId (fun objId => tmp (next objId) |>.1),
+    fun ⟨objId, vals⟩ => tmp (next objId) |>.2 vals⟩
+  | .destructor _ _ _ _ next => next.result
+  | .method _ _ _ _ next => next.result
+  | .fetch (n := a) objId next =>
+    let helper (prog : Program' lab ReturnType a)
+            : Σ (params : Program.Parameters.{0}), params.Product → ReturnType := Program'.result prog
+
+    ⟨.fetch objId (fun obj => helper (next obj) |>.1),
+      fun ⟨obj, vals⟩ => helper (next obj) |>.2 vals⟩
+  | .invoke (n := a) _ p next =>
+    let helper {ReturnType} (prog : Program' lab ReturnType a)
+            : Σ (params : Program.Parameters.{0}), params.Product → ReturnType := Program'.result prog
+    let ⟨pParams, pReturn⟩ := helper p
     let params :=
       pParams.append (fun pVals =>
-        result (n := m) (next (pReturn pVals)) |>.1)
+        helper (next (pReturn pVals)) |>.1)
     ⟨params, fun vals =>
       let ⟨pVals, vals'⟩ := Program.Parameters.splitProduct vals
-      result (n := m) (next (pReturn pVals)) |>.2 vals'⟩
-  | _, .return val => ⟨.empty, fun () => val⟩
+      result (next (pReturn pVals)) |>.2 vals'⟩
+  | .return val => ⟨.empty, fun () => val⟩
 
+def Program.result {lab : Ecosystem.Label} {ReturnType} (prog : Program lab ReturnType)
+  : Σ (params : Program.Parameters), params.Product → ReturnType :=
+  let ⟨_, prog'⟩ := prog
+  prog'.result
 
 /-- All body parameters - the parameters at the point of the return statement. -/
 def Program.params {lab ReturnType} [Inhabited ReturnType] (prog : Program lab ReturnType) : Program.Parameters :=
@@ -88,17 +99,17 @@ def Program.params {lab ReturnType} [Inhabited ReturnType] (prog : Program lab R
 def Program.returnValue {lab ReturnType} [Inhabited ReturnType] (prog : Program lab ReturnType) (vals : prog.params.Product) : ReturnType :=
   prog.result.2 vals
 
-def Program.map {lab : Ecosystem.Label} {A B : Type u} (f : A → B) (prog : Program lab A) : Program lab B :=
-  match prog with
-  | .constructor cid constrId args next =>
-    .constructor cid constrId args (fun x => map f (next x))
-  | .destructor cid destrId selfId args next =>
-    .destructor cid destrId selfId args (map f next)
-  | .method cid methodId selfId args next =>
-    .method cid methodId selfId args (map f next)
-  | .fetch objId next =>
-    .fetch objId (fun x => map f (next x))
-  | .invoke i p next =>
-    .invoke i p (fun x => map f (next x))
-  | .return val =>
-    .return (f val)
+-- def Program.map {lab : Ecosystem.Label} {A B : Type u} (f : A → B) (prog : Program lab A) : Program lab B :=
+--    match prog with
+--    | .constructor cid constrId args next =>
+--      .constructor cid constrId args (fun x => map f (next x))
+--    | .destructor cid destrId selfId args next =>
+--      .destructor cid destrId selfId args (map f next)
+--    | .method cid methodId selfId args next =>
+--      .method cid methodId selfId args (map f next)
+--    | .fetch objId next =>
+--      .fetch objId (fun x => map f (next x))
+--    | .invoke i p next =>
+--      .invoke i p (fun x => map f (next x))
+--    | .return val =>
+--      .return (f val)
