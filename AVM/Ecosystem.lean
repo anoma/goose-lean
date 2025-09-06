f83123be12fd78010482bf0c8c248025291cae25
Jan Mas Rovira <janmasrovira@gmail.com>
2025-09-06 10:03:52 +0200

    wip

--- AVM/Ecosystem.lean
+++ AVM/Ecosystem.lean
@@ -1,8 +1,10 @@
 import AVM.Class
 import AVM.Ecosystem.Label
+import AVM.Ecosystem.Member
 
 namespace AVM
 
 /-- Ecosystem is a collection of classes. -/
 structure Ecosystem (label : Ecosystem.Label) where
   classes : (c : label.ClassId) → Class c
+  functions : (f : label.FunctionId) → Function f
