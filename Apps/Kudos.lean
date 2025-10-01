d5fc8b51dcdb51051b34bb7db58ab82ff74ac3db
Lukasz Czajka <lukasz@heliax.dev>
2025-10-01 18:37:17 +0200

    kudos merge

--- Apps/Kudos.lean
+++ Apps/Kudos.lean
@@ -118,7 +118,32 @@ def clab : Class.Label where
     | Destructors.Burn => ⟨PUnit⟩
   DestructorSignatureId := Destructors.SignatureId
 
-def label : Ecosystem.Label := Ecosystem.Label.singleton clab
+inductive MultiMethods where
+  | Merge
+  deriving Repr, DecidableEq, FinEnum
+
+export MultiMethods (Merge)
+
+namespace Merge
+
+inductive ArgNames where
+  | Kudos1
+  | Kudos2
+  deriving DecidableEq, Repr, FinEnum
+
+export ArgNames (Kudos1 Kudos2)
+
+end Merge
+
+def label : Ecosystem.Label where
+  name := "Kudos"
+  ClassId := PUnit
+  classLabel := fun _ => clab
+  MultiMethodId := MultiMethods
+  MultiMethodObjectArgClass {f : MultiMethods} (_a : _) := match f with
+   | Merge => PUnit.unit
+  MultiMethodObjectArgNames : MultiMethods → Type := fun
+   | Merge => Merge.ArgNames
 
 def toObject (c : Kudos) : @ObjectData label .unit where
   quantity := c.quantity
@@ -129,12 +154,14 @@ def fromObject (o : @ObjectData label .unit) : Kudos :=
     quantity := o.quantity
     originator := o.privateFields.originator }
 
-instance hasIsObject : IsObject Kudos where
+instance instIsObjectOf : @IsObjectOf label () Kudos where
+  toObject := Kudos.toObject
+  fromObject := Kudos.fromObject
+
+instance instIsObject : IsObject Kudos where
   label := label
   classId := .unit
-  isObjectOf :=
-    { toObject := Kudos.toObject
-      fromObject := Kudos.fromObject }
+  isObjectOf := inferInstance
 
 def kudosMint : @Class.Constructor label .unit Constructors.Mint := defConstructor
   (body := fun (args : MintArgs) => ⟪
@@ -163,3 +190,32 @@ def kudosClass : @Class label .unit where
     | Methods.Transfer => kudosTransfer
   destructors := fun
     | Destructors.Burn => kudosBurn
+
+def kudosEcosystem : Ecosystem label where
+  classes := fun _ => kudosClass
+  multiMethods (f : MultiMethods) := match f with
+    | .Merge =>
+      let mergeArgsInfo (a : label.MultiMethodObjectArgNames Merge)
+      : ObjectArgInfo label Merge a :=
+      match a with
+      | .Kudos1 => { type := Kudos, isObjectOf := Kudos.instIsObjectOf }
+      | .Kudos2 => { type := Kudos, isObjectOf := Kudos.instIsObjectOf }
+
+      defMultiMethod label Merge
+        (argsInfo := mergeArgsInfo)
+        (body := fun kudos _args => ⟪
+                  let k1 := kudos .Kudos1
+                  let k2 := kudos .Kudos2
+                  return {
+                    assembled := {
+                      withOldUid _ _ := none
+                      withNewUid :=
+                        [{k1 with quantity := k1.quantity + k2.quantity : Kudos}]
+                    }
+                  }
+                ⟫)
+        (invariant := fun kudos _args _signatures =>
+                  let k1 := kudos .Kudos1
+                  let k2 := kudos .Kudos2
+                  k1.originator == k2.originator
+                  && k1.owner == k2.owner)
