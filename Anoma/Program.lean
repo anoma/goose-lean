a22f798ac51574a7f0c88d7be8f1149b5e0909d5
Jan Mas Rovira <janmasrovira@gmail.com>
2025-10-21 18:24:47 +0200

    remove commented out program instructions

--- Anoma/Program.lean
+++ Anoma/Program.lean
@@ -39,16 +39,6 @@ inductive Program : Type 2 where
   | queryResource (query : Program.ResourceQuery) (next : Resource → Program)
   | submitTransaction (tx : Transaction) (next : Program)
   | log (msg : String) (next : Program)
-  -- | decrypt (engine : EngineId) (data : Ciphertext) (next : Plaintext → Program)
-  -- | requestCommitment (engine : EngineId) (data : Signable) (next : Commitment → Program)
-  -- | generateIdentity (backend : Backend) (params : IDParams) (capabilities : Capabilities)
-  --     (next : (commitmentEngine : Option EngineId) → (decryptionEngine : Option EngineId) → (externalIdentity : EngineId) → Program)
-  -- | connectIdentity (externalIdentity : EngineId) (backend : Backend) (capabilities : Capabilities)
-  --     (next : (commitmentEngine : Option EngineId) → (decryptionEngine : Option EngineId) → Program)
-  -- | deleteIdentity (externalIdentity : EngineId) (backend : Backend) (next : Program)
-  -- | getValue (key : StorageKey) (next : Option StorageValue → Program)
-  -- | setValue (key : StorageKey) (value : StorageValue) (next : Program)
-  -- | deleteValue (key : StorageKey) (next : Program)
 
 def Program.genObjectId (next : ObjectId → Program) : Program :=
   Program.withRandomGen fun g =>
